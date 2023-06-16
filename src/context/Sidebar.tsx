87d68b4abea8db19c78fc7b812606205dfc34fe0
Aditya Chavan <aadityac73@gmail.com>
2023-06-16 22:07:39 +0530

    removed context

--- src/context/Sidebar.tsx
+++ src/context/Sidebar.tsx
@@ -1,30 +1,30 @@
-import React, { createContext, useContext, useState } from 'react';
+// import React, { createContext, useContext, useState } from 'react';
 
-interface TypeContext {
-  resource: string;
-  setResource: any;
-}
+// interface TypeContext {
+//   resource: string;
+//   setResource: any;
+// }
 
-const SideBarContext = createContext<TypeContext>({
-  resource: 'dashboard',
-  setResource: () => void 0
-});
+// const SideBarContext = createContext<TypeContext>({
+//   resource: 'dashboard',
+//   setResource: () => void 0
+// });
 
-export const useSidebar = () => {
-  return useContext(SideBarContext);
-};
+// export const useSidebar = () => {
+//   return useContext(SideBarContext);
+// };
 
-interface ProviderProps {
-  children: JSX.Element;
-}
+// interface ProviderProps {
+//   children: JSX.Element;
+// }
 
-const SidebarProvider: React.FC<ProviderProps> = ({ children }) => {
-  const [data, setData] = useState<string>('dashboard');
-  return (
-    <SideBarContext.Provider value={{ resource: data, setResource: setData }}>
-      {children}
-    </SideBarContext.Provider>
-  );
-};
+// const SidebarProvider: React.FC<ProviderProps> = ({ children }) => {
+//   const [data, setData] = useState<string>('dashboard');
+//   return (
+//     <SideBarContext.Provider value={{ resource: data, setResource: setData }}>
+//       {children}
+//     </SideBarContext.Provider>
+//   );
+// };
 
-export default SidebarProvider;
+// export default SidebarProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import SidebarProvider from './context/Sidebar.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <SidebarProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </SidebarProvider>
   </React.StrictMode>
 );
